53745599b6427696b0b81402fbbe00e0fbe02a70
Chiaki Kikkojin <kikkojin@artcast.jp>
2026-04-22 16:36:23 +0900

    Initial plugin scaffold

---
+++ README.md
@@ -0,0 +1,3 @@
+# DM Estimator
+
+WordPress 用の DM見積もりフォームプラグイン
---
+++ dm_estimator.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: DM Estimator
+ * Description: DM見積もりフォーム用の試作プラグイン
+ * Version: 0.1.0
+ * Author: Chiaki Kikkojin
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
